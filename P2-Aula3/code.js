2824a18ec880f28e356730ceacc3939d9169bd77
crickkin <37256927+crickkin@users.noreply.github.com>
2019-08-07 15:31:26 -0300

    P2 Aula 3 - Verificação de Email

--- P2-Aula3/code.js
+++ P2-Aula3/code.js
@@ -9,5 +9,22 @@ function CalculateAge() {
     let month = currentAge.getMonth();
     let day = currentAge.getDay();
 
-    text.innerHTML = "Sua idade é " + age + " anos, " + month + " meses e " + day + " dias";
+    text.innerHTML = `Sua idade é ${age} anos ${month} meses e ${day} dias`;
+}
+
+function VerifyEmail(){
+    let email = document.getElementById("email").value;
+    let text = document.getElementById('result');
+
+    if (email.indexOf('@') != -1){
+        let divArroba = email.split('@');
+        let divPonto = divArroba[1].split('.');
+            
+        text.innerHTML = (divArroba[0] != '' && divArroba[1].indexOf('.') != -1 && divPonto[1] != '')
+            ? 'Email Válido' 
+            : 'Email Não Válido';
+    }
+    else{
+        text.innerHTML = 'Email Não Válido'
+    }
 }
